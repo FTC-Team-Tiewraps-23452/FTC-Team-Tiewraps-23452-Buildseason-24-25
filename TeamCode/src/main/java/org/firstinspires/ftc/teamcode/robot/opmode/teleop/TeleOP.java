e0b632e2965488d9ee9485507997e3b0b6fce575
Medewerker <vksebastiaan@hotmail.com>
2024-10-21 15:40:54 +0200

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmode/teleop/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmode/teleop/TeleOP.java
@@ -84,7 +84,7 @@ public class TeleOP extends OpMode
 
         double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
         double x = gamepad1.left_stick_x;
-        double rx = gamepad1.right_stick_x;
+        double rx = -gamepad1.right_stick_x;
         mecanumDrivetrain.mecanumDrive(x,y,rx);
 
         // Show the elapsed game time and wheel power.
